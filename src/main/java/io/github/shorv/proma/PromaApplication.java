b1a684a0f4128c0d5ecf1365b0b1b7a69738ef48
shorv <kontakt.shorv@gmail.com>
2023-03-18 16:21:16 +0100

    disable security

--- src/main/java/io/github/shorv/proma/PromaApplication.java
+++ src/main/java/io/github/shorv/proma/PromaApplication.java
@@ -2,8 +2,9 @@ package io.github.shorv.proma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PromaApplication {
 
     public static void main(String[] args) {
